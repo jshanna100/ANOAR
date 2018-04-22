aa7c2fe9fcce6970c25b8d02bdc3d589876bb7aa
jshanna100 <jeff.hanna@gmail.com>
2018-04-22 18:45:22 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(name='anoar',
       author='Jeff Hanna',
       author_email='jeff.hanna@gmail.com',
       url='https://github.com/jshanna100/ANOAR/',
-      packages=['anoar.anoar']
+      packages=['anoar']
      )
