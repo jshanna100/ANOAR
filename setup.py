1ce43c7123444cd47a12fcb60f9348e7eaa0f8bf
jshanna100 <jeff.hanna@gmail.com>
2018-04-22 16:29:47 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from distutils.core import setup
 
 setup(name='anoar',
       version='1.0',
+      pymodules=["anoar"]
       description='Automated Non-Ocular Artefact Rejection',
       author='Jeff Hanna',
       author_email='jeff.hanna@gmail.com',
