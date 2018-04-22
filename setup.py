c4da57f88616d3ec55fbb3233e7ce855484a388d
jshanna100 <jeff.hanna@gmail.com>
2018-04-22 18:26:51 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(name='anoar',
       description='Automated Non-Ocular Artefact Rejection',
       author='Jeff Hanna',
       author_email='jeff.hanna@gmail.com',
-      url='https://github.com/jshanna100/ANOAR/'
+      url='https://github.com/jshanna100/ANOAR/',
       packages=['anoar']
      )
